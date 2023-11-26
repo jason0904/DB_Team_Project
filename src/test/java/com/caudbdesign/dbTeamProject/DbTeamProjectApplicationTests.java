da1e3ee92b4d8405a8af31e4a8ce13da49142cd6
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2023-11-26 23:31:37 +0900

    Initial Commit

---
+++ src/test/java/com/caudbdesign/dbTeamProject/DbTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caudbdesign.dbTeamProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbTeamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
