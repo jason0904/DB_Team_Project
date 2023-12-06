3d0c39ed0f12a93885a3d72e7dbdcf473cee33cf
polarpolar914 <kdw2003914@naver.com>
2023-12-06 21:20:10 +0900

    complete index page

--- front/plugins/persistedState.js
+++ front/plugins/persistedState.js
@@ -2,6 +2,7 @@ import createPersistedState from 'vuex-persistedstate';
 
 export default ({store}) => {
   createPersistedState({
-    paths: ['isLoggedIn', 'userData', 'userName']
+    paths: ['isLoggedIn', 'userData', 'userName', 'storedAccount', 'typeAccount', 'account_id', 'accoutNumbers'],
+    storage: window.localStorage,
   })(store);
 };
