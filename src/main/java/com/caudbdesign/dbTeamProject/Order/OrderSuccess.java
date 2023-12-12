a97c7de13fab950849374a084eb58db9b5752787
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2023-12-12 23:05:59 +0900

    Add ErrorChecking From Order and Order Amend

--- src/main/java/com/caudbdesign/dbTeamProject/Order/OrderSuccess.java
+++ src/main/java/com/caudbdesign/dbTeamProject/Order/OrderSuccess.java
@@ -26,11 +26,13 @@ public class OrderSuccess {
       portfolioRepository.updatePortfolio(item_id, quantity, account_id);
       if(market.equals("KOSPI")) {
         balanceRepository.updateBalance(account_id, balanceRepository.selectBalance(account_id).getTotal_Balance() + quantity * limit_price, balanceRepository.selectBalance(account_id).getKRW_Balance() + quantity * limit_price ,balanceRepository.selectBalance(account_id).getUSD_Balance());
-        portfolioRepository.updateStockPortfolioTotalPurchasePrice(item_id, account_id, portfolioRepository.selectPortfolio(account_id).getTotal_purchase_price() + quantity * limit_price);
+        portfolioRepository.updateStockPortfolioTotalPurchasePrice(item_id, account_id, portfolioRepository.selectPortfoliobyItemId(account_id, item_id).getTotal_purchase_price()
+            + quantity * limit_price);
       } else {
         float KRWExchange = limit_price * exchangeRepository.selectRate("USD", "KRW").getCurrent_exchange_rate();
         balanceRepository.updateBalance(account_id, balanceRepository.selectBalance(account_id).getTotal_Balance() + quantity * KRWExchange, balanceRepository.selectBalance(account_id).getKRW_Balance(), balanceRepository.selectBalance(account_id).getUSD_Balance() + quantity * limit_price);
-        portfolioRepository.updateStockPortfolioTotalPurchasePrice(item_id, account_id, portfolioRepository.selectPortfolio(account_id).getTotal_purchase_price() + quantity * limit_price);
+        portfolioRepository.updateStockPortfolioTotalPurchasePrice(item_id, account_id, portfolioRepository.selectPortfoliobyItemId(account_id, item_id).getTotal_purchase_price()
+            + quantity * limit_price);
       }
     } catch (InterruptedException e) {
       e.printStackTrace();
