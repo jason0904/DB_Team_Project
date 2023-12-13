80ddffdfca66250ca3222d05abf1fd3132ff56ab
Jeshin Kim <37035547+jason0904@users.noreply.github.com>
2023-12-13 14:53:57 +0900

    add Slf4j to trace admin log

--- src/main/java/com/caudbdesign/dbTeamProject/Admin/Analystinfo/AnalystController.java
+++ src/main/java/com/caudbdesign/dbTeamProject/Admin/Analystinfo/AnalystController.java
@@ -3,12 +3,14 @@ package com.caudbdesign.dbTeamProject.Admin.Analystinfo;
 
 import com.caudbdesign.dbTeamProject.Item.Stock.AnalystInfo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
+@Slf4j
 @RequiredArgsConstructor
 public class AnalystController {
 
--- src/main/java/com/caudbdesign/dbTeamProject/Admin/ChangePrice/PriceController.java
+++ src/main/java/com/caudbdesign/dbTeamProject/Admin/ChangePrice/PriceController.java
@@ -1,12 +1,14 @@
 package com.caudbdesign.dbTeamProject.Admin.ChangePrice;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 @RequiredArgsConstructor
 public class PriceController {
 
